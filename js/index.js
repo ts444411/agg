4efd54ebff337bc991a91268e7853ae257c6b7f9
ts444411 <166647693+ts444411@users.noreply.github.com>
2024-04-10 21:10:20 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 function signIn() {
   const pws = [133127726, 71319];
   let pwsbool = prompt("Enter the AGG password(s)", "Type Password Here");
-  if (pwsbool == 13
+  if (pwsbool == pws[0] || pwsbool == pws[1]) {
+    alert("Sign in successful!);
+  } else {
+    alert("wrong password!");
+  }
 }
-signIn();
