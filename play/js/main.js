36eca3fdff42636b6cbdea20311ad78b94523bc5
ts444411 <166647693+ts444411@users.noreply.github.com>
2024-04-11 20:20:02 -0400

    Add files via upload

---
+++ play/js/main.js
@@ -0,0 +1,11 @@
+function tcrack() {
+    window.location.href = "apps/tcrack.html";
+}
+
+function lol() {
+    window.location.href = "apps/1v1lol.html";
+}
+
+function mine() {
+    window.location.href = "apps/minecraft.html";
+}
