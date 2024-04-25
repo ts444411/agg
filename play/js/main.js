f5ad527b2f964e060bec792daf88529e6af833c8
ts444411 <166647693+ts444411@users.noreply.github.com>
2024-04-25 11:53:19 -0400

    Create main.js

---
+++ play/js/main.js
@@ -0,0 +1,30 @@
+function tcrack() {
+    window.location.href = "apps/tcrack.html";
+}
+
+function lol() {
+    window.location.href = "apps/1v1lol/index.html";
+}
+
+function mine() {
+    window.location.href = "apps/minecraft.html";
+}
+
+function trex() {
+    window.location.href = "apps/trex/index.html";
+}
+
+function slope() {
+    window.location.href = "apps/slope.html"
+}
+
+function tofe() {
+    window.location.href = "apps/2048.html"
+}
+
+function snake() {
+    window.location.href = "apps/snake.html"
+}
+function ovo() {
+    window.location.href = "apps/ovo/index.html"
+}
